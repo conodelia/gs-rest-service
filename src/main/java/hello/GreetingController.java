c68ea76a9a74c87ad8d55746898ac1988b509d37
Cono D'Elia <cono.delia@capco.com>
2018-11-22 11:16:51 -0500

    Initial

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        System.out.println("Hello1");
+        System.out.println("Hello12");
         return new Greeting(counter.incrementAndGet(),
                            String.format(template, name));
     }
